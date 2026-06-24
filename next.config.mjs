ab210beec89d0b7d2cc12716091637d3e0531c70
bkknews <thanakrit.sibunruang@gmail.com>
2026-06-24 22:47:54 +0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: 'res.cloudinary.com' },
       { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'image.oliveyoung.co.kr' },
     ],
   },
   async redirects() {
